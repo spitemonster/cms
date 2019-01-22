63db9777b49474388224810108b4426a89ec33b0
Kirby Johnston <info@spitemonster.com>
2019-01-22 17:48:06 -0500

    initial commit a day late

---
+++ resources/vue/App.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+    <main>
+      <draggable element="form" :list="fields">
+          <template v-for="field in fields">
+            <fieldCard :field="field"></fieldCard>
+          </template>
+      </draggable>
+
+      <select name="field-type" id="fieldSelector">
+          <option value="text">Text</option>
+          <option value="textarea">Text Area</option>
+      </select>
+
+      <button @click="addField">Add Field</button>
+    </main>
+</template>
+<script>
+    import fieldCard from './components/fieldCard.vue'
+    import draggable from 'vuedraggable'
+
+    export default {
+      name: 'App',
+      data () {
+        return {
+          fields: [{id: 0, type: 'text'}, {id: 1, type: 'textarea'}, {id: 2, type: 'text'}]
+        }
+      },
+      methods: {
+        addField () {
+          let sel = document.getElementById('fieldSelector')
+
+          this.fields.push({id: this.fields.length, type: sel.value})
+        }
+      },
+      components: {
+        fieldCard,
+        draggable
+      },
+      updated () {
+        this.fields.forEach((field) => {
+          console.log(field.id)
+        })
+      }
+    }
+</script>
+<style lang="css">
+
+</style>
---
+++ resources/vue/components/fieldCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <fieldset class="field-card">
+          <input type="text" v-model="FieldName" placeholder="Enter Field Name" class="field-card__name" />
+          <div class="field-card__meta">
+              <span>Type: {{ field.type }}</span>
+              <span>ID: {{ field.id }}</span>
+          </div>
+    </fieldset>
+</template>
+<script>
+    export default {
+      name: 'inputField',
+      data () {
+        return {
+          FieldName: ''
+        }
+      },
+      props: ['field']
+    }
+</script>
+<style lang="scss">
+</style>
